1659e667d95685f3b92a60b9bd246c04d269d58b
unknown <marlil@stud.ntnu.no>
2018-12-06 22:03:53 +0100

    Almost done with oppgave 6

--- src/no/ntnu/imt3281/weather/Forecast.java
+++ src/no/ntnu/imt3281/weather/Forecast.java
@@ -1,15 +1,13 @@
 package no.ntnu.imt3281.weather;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
+import javax.xml.xpath.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +21,9 @@ public class Forecast {
         Document doc = null;
 
         try {
+            ArrayList<String> forecast = new ArrayList<>();
+            List<String> title = new ArrayList<>();
+            List<String> body = new ArrayList<>();
             String base = "/weatherdata/forecast/text/location/time/";
             builder = factory.newDocumentBuilder();
             doc = builder.parse(url);
@@ -33,7 +34,20 @@ public class Forecast {
 
             XPathExpression expr = xpath.compile(base +"title/text()");
 
-            
+           NodeList titles = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+
+           for(int i = 0; i < titles.getLength();i++) {
+               title.add(titles.item(i).getNodeValue());
+           }
+
+            expr = xpath.compile(base +"body/text()");
+
+            NodeList bodies = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+
+            for(int i = 0; i < titles.getLength();i++) {
+                body.add(bodies.item(i).getNodeValue());
+            }
+
 
         } catch (ParserConfigurationException | SAXException | IOException | XPathExpressionException e) {
 
