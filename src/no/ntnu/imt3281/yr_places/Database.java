e0adeb4e30c004b8d4a5a31ec182f659eba8d850
unknown <marlil@stud.ntnu.no>
2018-12-06 15:03:49 +0100

    Working on oppgave 3

--- src/no/ntnu/imt3281/yr_places/Database.java
+++ src/no/ntnu/imt3281/yr_places/Database.java
@@ -1,30 +1,36 @@
 package no.ntnu.imt3281.yr_places;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Database {
 
     private Connection dbCon;
 
-    public Database(){
-        this.dbCon = memDB();
+    public Database(String c){
+        if (c =="T") {
+            this.dbCon = memDB();
+        }
+        else {
+            this.dbCon = connectDB();
+        }
     }
+
+    public Database getDB
 
     private Connection memDB() {
         try (Connection connect = DriverManager.getConnection("jdbc:derby:memory:yrDB;create=true")) {
             System.out.println("DATABASE: Created");
             String sql = "CREATE TABLE PLACES\n" +
                     "(Kommunenr INTEGER NOT NULL, \n" +
-                    "StedsNavn VARCHAR(64) NOT NULL PRIMARY KEY, \n" +
+                    "StedsNavn VARCHAR(64) NOT NULL, \n" +
+                    "Prioritet INTEGER NOT NULL, \n" +
                     "StedsType VARCHAR(64) NOT NULL, \n" +
                     "Kommune VARCHAR(64) NOT NULL, \n" +
                     "Fylke VARCHAR(64) NOT NULL, \n" +
                     "Latitude FLOAT NOT NULL, \n" +
                     "Longitude FLOAT NOT NULL, \n" +
-                    "URL VARCHAR(128) NOT NULL)";
+                    "URL VARCHAR(128) NOT NULL, \n" +
+                    "CONSTRAINT PK_Place PRIMARY KEY(StedsNavn, Prioritet)";
             Statement stmnt = connect.createStatement();
             stmnt.execute(sql);
             stmnt.close();
@@ -51,7 +57,7 @@ public class Database {
             System.out.println("DATABASE: Created");
             String sql = "CREATE TABLE PLACES\n" +
                     "(Kommunenr INTEGER NOT NULL, \n" +
-                    "StedsNavn VARCHAR(64) NOT NULL PRIMARY KEY, \n" +
+                    "StedsNavn VARCHAR(64) NOT NULL, \n" +
                     "StedsType VARCHAR(64) NOT NULL, \n" +
                     "Kommune VARCHAR(64) NOT NULL, \n" +
                     "Fylke VARCHAR(64) NOT NULL, \n" +
@@ -70,7 +76,25 @@ public class Database {
     }
 
     public void addPlace(Place p) {
+        try(Connection connect = DriverManager.getConnection("jdbc:derby:yrDB")) {
+            String sql = "INSERT INTO PLACES (Kommunenr, StedsNavn, StedsType, Kommune, Fylke, Latitude, Longitude, URL) VALUES (?,?,?,?,?,?,?,?)";
+            PreparedStatement stmnt = connect.prepareStatement(sql);
+            stmnt.setInt(1, p.getKommunenr());
+            stmnt.setString(2, p.getStedsnavn());
+            stmnt.setString(3, p.getStedstype());
+            stmnt.setString(4, p.getKommune());
+            stmnt.setString(5, p.getFylke());
+            stmnt.setFloat(6, p.getLat());
+            stmnt.setFloat(7, p.getLng());
+            stmnt.setString(8, p.getVarselURL());
 
+        } catch (SQLException e) {
+            System.out.println("DATABASE: ERROR no connection");
+        }
+    }
+
+    public Place findPlace (float lng, float lat) {
+        return null;
     }
 
 }
--- test/no/ntnu/imt3281/yr_places/TestDatabase.java
+++ test/no/ntnu/imt3281/yr_places/TestDatabase.java
@@ -2,12 +2,35 @@ package no.ntnu.imt3281.yr_places;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class TestDatabase {
 
     @Test
-    public void testDatabase(){
-        Database db = new Database();
+    public void testAddPlace(){
+        Database db = new Database("T");
+        Place p = null;
+        try {
+           p = new Place(AllThePlaces.getAllPlaces().get(8197));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        db.addPlace(p);
 
+        try (Connection connect = DriverManager.getConnection("jdbc:derby:memory:yrDB")) {
+            Statement stmnt = connect.createStatement();
+            String sql = "FROM PLACES SELECT StedsNavn" +
+                    "WHERE StedsNavn LIKE Ankenes AND Prioritet LIKE 55";
+            String place = stmnt.execute(sql);
+            stmnt.close();
 
+        } catch (SQLException e) {
+
+        }
     }
 }
