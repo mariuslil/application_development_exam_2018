6b3f91a2afde4677d98f4139746a858bf56cd8ab
unknown <marlil@stud.ntnu.no>
2018-12-06 11:01:14 +0100

    started oppgave 2

---
+++ src/no/ntnu/imt3281/yr_places/AllThePlaces.java
@@ -0,0 +1,13 @@
+package no.ntnu.imt3281.yr_places;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllThePlaces {
+
+    public static void getAllPlaces(){
+        ArrayList<ArrayList<String>> places = new ArrayList<ArrayList<String>>();
+        ArrayList<String> place = new ArrayList<>();
+        
+    }
+}
