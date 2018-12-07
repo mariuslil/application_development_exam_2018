b79b47968b20010ad24cdfd136c48c1588a42000
unknown <marlil@stud.ntnu.no>
2018-12-07 09:58:42 +0100

    Working on findPos in oppgave 8

--- src/no/ntnu/imt3281/click_areas/CreateClickAreas.java
+++ src/no/ntnu/imt3281/click_areas/CreateClickAreas.java
@@ -1,13 +1,19 @@
 package no.ntnu.imt3281.click_areas;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polyline;
 
+import javax.security.auth.callback.Callback;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateClickAreas {
 
@@ -17,7 +23,7 @@ public class CreateClickAreas {
     }
 
     @FXML
-    private ListView<?> points;
+    private ListView<String> points;
 
     @FXML
     private TextField areaName;
@@ -44,14 +50,19 @@ public class CreateClickAreas {
     }
 
     public void addArea(javafx.event.ActionEvent actionEvent) {
+
     }
 
     public void toFile(javafx.event.ActionEvent actionEvent) {
+
     }
 
     public void findPos(javafx.scene.input.MouseEvent mouseEvent) {
-        double x = mouseEvent.getScreenX();
+        double x = mouseEvent.getSceneX();
         double y = mouseEvent.getSceneY();
+        points.getItems().add(x+" : "+y);
+        Polyline line = new Polyline();
+        line.getPoints().addAll(x,y);
 
     }
 }
